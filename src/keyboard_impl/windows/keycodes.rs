681aae4fa070cd391022b26d5be65353184cc7e6
chiehw <todaymofish@gmail.com>
2023-03-15 16:58:29 -0700

    Get code by rdev Key

--- src/keyboard_impl/windows/keycodes.rs
+++ src/keyboard_impl/windows/keycodes.rs
@@ -1,31 +1,37 @@
 use crate::keyboard_impl::KeyOps;
+use hbb_common::{
+    anyhow::{self, anyhow},
+    ResultType,
+};
 use rdev::Key;
 
 impl KeyOps for Key {
-    fn from_pos(code: u32) -> Option<Key> {
-        let key = rdev::win_key_from_scancode(code);
-        match key {
-            Key::Unknown(_) => None,
-            _ => Some(key),
+    fn pos_code(&self) -> ResultType<u32> {
+        rdev::win_scancode_from_key(*self)
+            .ok_or_else(|| anyhow!("Failed to get scancode for {:?}", self))
+    }
+
+    fn sys_code(&self) -> ResultType<u32> {
+        rdev::win_keycode_from_key(*self)
+            .ok_or_else(|| anyhow!("Failed to get vk code for {:?}", self))
+    }
+
+    fn from_pos(code: u32) -> ResultType<Key> {
+        match rdev::win_key_from_scancode(code) {
+            Key::Unknown(code) => anyhow::bail!("Unknown scancode: {}", code),
+            key => Ok(key),
         }
     }
 
-    fn from_event(event: &rdev::Event) -> Option<Key> {
+    fn from_event(event: &rdev::Event) -> ResultType<Key> {
         let code = event.scan_code;
         Self::from_pos(code)
-    }
-
-    fn pos_code(&self) {
-        todo!()
-    }
-
-    fn sys_code(&self) {
-        todo!()
     }
 }
 
 #[test]
-fn test_from_code() {
-    // ShiftLeft: scancode = 42(0x2A)
-    assert_eq!(Some(Key::ShiftLeft), Key::from_pos(42))
+fn test_from_code() -> ResultType<()> {
+    // ShiftLeft: scancode = 42(0x2A) in Windows
+    assert_eq!(Key::ShiftLeft, Key::from_pos(42)?);
+    Ok(())
 }
